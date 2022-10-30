a6faa5dd6fd4fcac0ed532df59adc495a1227590
vanvans03-def <teerahitchana@gmail.com>
2022-10-30 12:05:40 +0700

    update category service

---
+++ services/categories.service.js
@@ -0,0 +1,68 @@
+const { response } = require("express");
+const { category } = require("../models/category.model")
+
+async function createCategory(params,callback){
+    if(!params.categoryName){
+        return callback(
+            {
+            message:"Category Name Required"
+            },
+            ""
+        );
+    }
+
+const model = new category(params);
+model
+    .save()
+    .then((response) =>{
+        return callback(null,response);
+    })
+    .catch((error) => {
+        return callback(error);
+    });
+}
+
+async function getCategories(params,callback){
+    const categoryName = params.categoryName;
+    var condition = categoryName ? {
+        categoryName: { $regex: new RegExp(categoryName),$options:"i"},
+    }
+    :{};
+
+    let perPage = Math.abs(params.pageSize) || MONGO_DB_CONFIG.pageSize
+    let page = (Math.abs(params.page) || 1) - 1;
+
+    category
+    .find(condition,"categoryName categoryImage")
+    .limit(perPage)
+    .skip(perPage * page)
+    .then((response) => {
+        return callback(null,response);
+    })
+    .catch((error) => {
+        return callback(error);
+    });
+}
+
+
+async function getCategoriesById(params,callback){
+    const categoryName = params.categoryName;
+    var condition = categoryName ? {
+        categoryName: { $regex: new RegExp(categoryName),$options:"i"},
+    }
+    :{};
+    
+    let perPage = Math.abs(params.pageSize) || MONGO_DB_CONFIG.pageSize
+    let page = (Math.abs(params.page) || 1) - 1;
+
+    category
+    .find(condition,"categoryName categoryImage")
+    .limit(perPage)
+    .skip(perPage * page)
+    .then((response) => {
+        return callback(null,response);
+    })
+    .catch((error) => {
+        return callback(error);
+    });
+}
