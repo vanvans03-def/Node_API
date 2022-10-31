51390d66b777ce335ae3f203eeb3da32e68ee385
vanvans03-def <teerahitchana@gmail.com>
2022-10-31 19:48:54 +0700

    add fucn to Json

--- models/category.model.js
+++ models/category.model.js
@@ -3,19 +3,29 @@ const mongoose = require("mongoose");
 const category = mongoose.model(
     "Category",
     mongoose.Schema({
-        categoryName:{
-            type: String ,
-            required:true,
-            unique:true,
-        },
-        categoryDescription:{
+        categoryName: {
             type: String,
-            required:false
+            required: true,
+            unique: true,
         },
-        categoryImage:{
+        categoryDescription: {
+            type: String,
+            required: false
+        },
+        categoryImage: {
             type: String,
         },
-    })
+        },
+        {
+            toJSON: {
+                transform: function (doc, ret) {
+                    ret.categoryId = ret._id.toString();
+                    delete ret._id;
+                    delete ret._v;
+                },
+            },
+        }
+    )
 );
 
 module.exports = {
