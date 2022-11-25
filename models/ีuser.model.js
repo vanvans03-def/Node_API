fcc08dfb51cc0599a97c5eafbfe823a12b1d4181
vanvans03-def <teerahitchana@gmail.com>
2022-11-25 17:08:57 +0700

    add usermodel

---
+++ models/ีuser.model.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+
+const user = mongoose.model{
+    "User",
+    mongoose.Schema(
+        {
+        fullName:{
+            type: String,
+            required: true,
+            unique:true
+        },
+        email:{
+            type:String,
+            required:true,
+            unique:true
+        },
+        password:{
+            type:String,
+            required:true
+        }
+    },
+        {
+            toJSON:{
+                transform: function (doc,ret){
+                    ret.userId = ret._id.toString();
+                    delete ret._id;
+                    delete ret._v;
+                    delete ret.password;
+                }
+            }
+        }, {
+            timestamp: true
+        })
+};
+module.exports = {
+    user
+}
