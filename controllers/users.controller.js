79767a9b1c0e7def6cff0d02b882d89f3ca720ad
TEERA HITCHANA <teerahitchana@gmail.com>
2022-11-28 17:50:41 +0700

    fix bug at login function

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,22 +1,21 @@
 const userService = require("../services/users.service");
 
 exports.register = (req, res, next) => {
-    userServices.register(req.body, (error, results) => {
+    userService.register(req.body, (error, results) => {
         if (error) {
             return next(error);
         }
-        else {
-            return res.status(200).send({
-                message: "Success",
-                data: results
-            });
-        }
+
+        return res.status(200).send({
+            message: "Success",
+            data: results
+        });
     });
 };
 
 exports.login = (req, res, next) => {
     const { email, password } = req.body;
-    userServices.login({ email, password }, (error, results) => {
+    userService.login({ email, password }, (error, results) => {
         if (error) {
             return next(error);
         }
