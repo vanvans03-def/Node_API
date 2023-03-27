5050371757ca0a7c2baad2ac3ce08851d586e8ca
vanvans03-def <teerahitchana@gmail.com>
2023-03-27 16:06:39 +0700

    a

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const TOKEN_KEY = "RANDOM_KEY";
 
 function authenticationToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
+    const authHeader = req.headers['x-auth-token'];
     const token = authHeader && authHeader.split(' ')[1];
 
     if (token == null) return res.sendStatus(401);
--- services/users.service.js
+++ services/users.service.js
@@ -1,10 +1,10 @@
-const { User } = require("../models/user.model");
+const { user } = require("../models/user.model");
 const bcrypt = require("bcryptjs");
 const auth = require("../middleware/auth");
 const { Product } = require("../models/product.model");
 
 async function login({ email, password }, callback) {
-    const userModel = await User.findOne({email}).select("+password")
+    const userModel = await user.findOne({email}).select("+password")
    
 
 
@@ -31,7 +31,7 @@ async function register(params, callback) {
         });
     }
 
-    let isUserExist = await User.findOne({email:params.email});
+    let isUserExist = await user.findOne({email:params.email});
 
     if (isUserExist) {
         return callback({
@@ -42,7 +42,7 @@ async function register(params, callback) {
     const salt = bcrypt.genSaltSync(10);
     params.password = bcrypt.hashSync(params.password, salt);
 
-    const userSchema = new User(params);
+    const userSchema = new user(params);
     userSchema.save()
         .then((response) => {
             return callback(null, response);
@@ -51,34 +51,32 @@ async function register(params, callback) {
             return callback(error);
         });
 }
-
-
 async function addToCart(userId, productId) {
     try {
       const product = await Product.findById(productId);
-      let user = await User.findById(userId);
+      let user1 = await user.findById(userId);
   
-      if (user.cart.length == 0) {
-        user.cart.push({ product, quantity: 1 });
+      if (user1.cart.length == 0) {
+        user1.cart.push({ product, quantity: 1 });
       } else {
         let isProductFound = false;
-        for (let i = 0; i < user.cart.length; i++) {
-          if (user.cart[i].product._id.equals(product._id)) {
+        for (let i = 0; i < user1.cart.length; i++) {
+          if (user1.cart[i].product._id.equals(product._id)) {
             isProductFound = true;
           }
         }
   
         if (isProductFound) {
-          let producttt = user.cart.find((productt) =>
+          let producttt = user1.cart.find((productt) =>
             productt.product._id.equals(product._id)
           );
           producttt.quantity += 1;
         } else {
-          user.cart.push({ product, quantity: 1 });
+          user1.cart.push({ product, quantity: 1 });
         }
       }
-      user = await user.save();
-      return user;
+      user1 = await user1.save();
+      return user1;
     } catch (e) {
       throw new Error(e.message);
     }
