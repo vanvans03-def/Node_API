733af6dfe742b1742d452be05ffc2a779263f6d7
vanvans03-def <teerahitchana@gmail.com>
2023-03-27 16:11:25 +0700

    c

--- services/users.service.js
+++ services/users.service.js
@@ -54,29 +54,29 @@ async function register(params, callback) {
 async function addToCart(userId, productId) {
     try {
       const product = await Product.findById(productId);
-      let user1 = await user.findById(userId);
+      let userModel = await user.findById(userId);
   
-      if (user1.cart.length == 0) {
-        user1.cart.push({ product, quantity: 1 });
+      if (userModel.cart.length == 0) {
+        userModel.cart.push({ product, quantity: 1 });
       } else {
         let isProductFound = false;
-        for (let i = 0; i < user1.cart.length; i++) {
-          if (user1.cart[i].product._id.equals(product._id)) {
+        for (let i = 0; i < userModel.cart.length; i++) {
+          if (userModel.cart[i].product._id.equals(product._id)) {
             isProductFound = true;
           }
         }
   
         if (isProductFound) {
-          let producttt = user1.cart.find((productt) =>
+          let producttt = userModel.cart.find((productt) =>
             productt.product._id.equals(product._id)
           );
           producttt.quantity += 1;
         } else {
-          user1.cart.push({ product, quantity: 1 });
+            userModel.cart.push({ product, quantity: 1 });
         }
       }
-      user1 = await user1.save();
-      return user1;
+      userModel = await userModel.save();
+      return userModel;
     } catch (e) {
       throw new Error(e.message);
     }
@@ -85,19 +85,19 @@ async function addToCart(userId, productId) {
   async function removeFromCart(userId, productId) {
     try {
       const product = await Product.findById(productId);
-      let user = await User.findById(userId);
+      let userModel = await user.findById(userId);
   
-      for (let i = 0; i < user.cart.length; i++) {
-        if (user.cart[i].product._id.equals(product._id)) {
-          if (user.cart[i].quantity == 1) {
-            user.cart.splice(i, 1);
+      for (let i = 0; i < userModel.cart.length; i++) {
+        if (userModel.cart[i].product._id.equals(product._id)) {
+          if (userModel.cart[i].quantity == 1) {
+            userModel.cart.splice(i, 1);
           } else {
-            user.cart[i].quantity -= 1;
+            userModel.cart[i].quantity -= 1;
           }
         }
       }
-      user = await user.save();
-      return user;
+      userModel = await userModel.save();
+      return userModel;
     } catch (e) {
       throw new Error(e.message);
     }
